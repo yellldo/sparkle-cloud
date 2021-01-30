0499984336b41e48d174b90ae8d4b04ca53b8ce0
shenjia <shenjia@bjrrtx.com>
2021-01-30 22:47:37 +0800

    fix:服务注册

---
+++ sparkle-authorization/src/main/java/com/sparkle/AuthApplication.java
@@ -0,0 +1,18 @@
+package com.sparkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author sj
+ * @version 1.0.0 2021/1/30
+ * @className: AuthApplication
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
