c2a636108007e6275f38e77b8ab59cf082a087e8
shenjia <shenjia@bjrrtx.com>
2021-01-30 22:29:31 +0800

    fix:服务注册

--- sparkle-consumer/src/main/java/com/sparkle/SparkleConsumerApplication.java
+++ sparkle-consumer/src/main/java/com/sparkle/SparkleConsumerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -15,8 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date : 2020/7/9 16:57
  **/
 @SpringBootApplication(scanBasePackages = "com.sparkle", exclude = DataSourceAutoConfiguration.class)
-@EnableDiscoveryClient
-@EnableFeignClients
+@EnableEurekaClient
 public class SparkleConsumerApplication {
 
     public static void main(String[] args) {
