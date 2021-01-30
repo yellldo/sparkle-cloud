9b5898ce7de75e05c74115b6b45158fd4f105a4a
shenjia <shenjia@bjrrtx.com>
2021-01-31 00:42:55 +0800

    fix:服务注册

--- sparkle-gateway/src/main/java/com/sparkle/SparkleGateWayApplication.java
+++ sparkle-gateway/src/main/java/com/sparkle/SparkleGateWayApplication.java
@@ -2,7 +2,8 @@ package com.sparkle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * ClassName : SparkleGateWayApplication<br>
@@ -13,7 +14,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @date : 2020/7/9 17:17
  **/
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableZuulProxy
 public class SparkleGateWayApplication {
 
     public static void main(String[] args) {
