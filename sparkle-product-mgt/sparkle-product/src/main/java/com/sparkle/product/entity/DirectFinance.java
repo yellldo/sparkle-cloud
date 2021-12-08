7138578955037aae47d059eec4fb4f0b5a60f1c5
shenjia <shenjia@bjrrtx.com>
2021-12-08 10:31:20 +0800

    fix:实体提交

--- sparkle-product-mgt/sparkle-product/src/main/java/com/sparkle/product/entity/DirectFinance.java
+++ sparkle-product-mgt/sparkle-product/src/main/java/com/sparkle/product/entity/DirectFinance.java
@@ -4,15 +4,12 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
 import java.io.Serializable;
 import java.util.Date;
-
 import lombok.Data;
 
 /**
  * 直接融资产品表
- *
  * @TableName direct_finance
  */
 @TableName(value = "direct_finance")
